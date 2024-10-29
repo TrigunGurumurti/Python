d4678a5c980a699d26e0471ad61b382cbadd7fd1
TrigunGurumurti <triguntamragouri@gmail.com>
2024-10-29 18:56:37 +0530

    strings

---
+++ strings.py
@@ -0,0 +1,13 @@
+# Printing String 1   
+String1 = "Program 1"
+print("First String : ",String1)  
+
+
+# Printing String 2
+String2 = "Program 2"
+print("Second String : ",String2)  
+ 
+
+# Printing String 3
+String3 = "Program 3"
+print("Third String : ",String3)
